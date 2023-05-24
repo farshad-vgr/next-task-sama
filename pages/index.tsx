08677b22ed3ed91ee8741eb032c2a7559f665ef2
farshad-vgr <farshadvaghari@gmail.com>
2023-05-24 12:28:04 +0330

    index file static props deleted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,6 @@
 import Head from "next/head";
 
-interface Props {
-	cities: {
-		id: number;
-		name: string;
-		province: string;
-	}[];
-}
-
-export default function Home({ cities }: Props) {
-	console.log(cities);
+export default function Home() {
 
 	return (
 		<>
@@ -20,17 +11,4 @@ export default function Home({ cities }: Props) {
 			<div style={{ minHeight: "500px" }}>This is for test HOME page</div>
 		</>
 	);
-}
-
-export async function getStaticProps() {
-	// Fetching data from internal "API route"
-	// const response = await fetch(`https://fv-travelo-iran.vercel.app/api/cities?country=iran`);
-	const response = await fetch(`http://localhost:3000/api/cities?country=iran`);
-	const cities = await response.json();
-
-	return {
-		props: {
-			cities,
-		},
-	};
 }
