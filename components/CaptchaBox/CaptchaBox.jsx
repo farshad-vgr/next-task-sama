95b50700c8c22d576a7b92151fa680b102263162
farshad-vgr <farshadvaghari@gmail.com>
2023-05-26 20:58:45 +0330

    testing expiration callback functions

--- components/CaptchaBox/CaptchaBox.jsx
+++ components/CaptchaBox/CaptchaBox.jsx
@@ -7,10 +7,25 @@ const CaptchaBox = ({ setIsDisable }) => {
 	const getToken = (token) => {
 		token ? setIsDisable(false) : null;
 	};
+
+	const tokenExpired = () => {
+		console.log("token expired!");
+	};
+
+	const challangeExpired = () => {
+		console.log("challange expired!");
+	};
+
 	return (
 		<>
 			<div className="self-center">
-				<ArcaptchaWidget ref={ArRef} site-key="i6qqnsxay6" callback={getToken} />
+				<ArcaptchaWidget
+					ref={ArRef}
+					site-key="i6qqnsxay6"
+					callback={getToken}
+					expired_callback={tokenExpired}
+					chlexpired_callback={challangeExpired}
+				/>
 			</div>
 		</>
 	);
