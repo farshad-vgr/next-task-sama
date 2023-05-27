b4eea6a590098629d0c57cf3415023fba3ff33a1
farshad-vgr <farshadvaghari@gmail.com>
2023-05-27 09:46:37 +0330

    some padding added to text

--- components/CopyRight/CopyRight.tsx
+++ components/CopyRight/CopyRight.tsx
@@ -1,15 +1,15 @@
 export default function CopyRight() {
 	return (
 		<>
-			<div className="absolute bottom-0 flex items-center justify-center w-full text-white -translate-x-1/2 rounded cursor-default left-1/2 bg-dark">
+			<div className="absolute bottom-0 flex items-center justify-center py-[2px] w-full text-white -translate-x-1/2 rounded cursor-default left-1/2 bg-dark">
 				<span className="hidden sm:block">کلیه حقوق مادی و معنوی این محصول متعلق به شرکت</span>{" "}
 				<i className="px-2">
 					{" "}
-					<a href="https://samarayaneh.ir/" target="_blank" className="text-blue-400 hover:text-blue-300 hover:underline">
+					<a href="https://samarayaneh.ir/" target="_blank" className="py-[2px] text-blue-400 hover:text-blue-300 hover:underline">
 						سما رایانه
 					</a>
 				</i>{" "}
-				<span className="hidden sm:block me-1">میباشد</span>© {new Date().getFullYear()}
+				<span className="py-[2px] hidden sm:block me-1">میباشد</span>© {new Date().getFullYear()}
 			</div>
 		</>
 	);
