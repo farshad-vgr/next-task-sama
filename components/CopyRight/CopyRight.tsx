0f507da78710485e1f7c099a09c12fea1e94c7d3
farshad-vgr <farshadvaghari@gmail.com>
2023-05-27 10:08:29 +0330

    footer padding edited

--- components/CopyRight/CopyRight.tsx
+++ components/CopyRight/CopyRight.tsx
@@ -2,14 +2,14 @@ export default function CopyRight() {
 	return (
 		<>
 			<div className="absolute bottom-0 flex items-center justify-center py-1 w-full text-white -translate-x-1/2 rounded cursor-default left-1/2">
-				<span className="py-1 hidden sm:block">کلیه حقوق مادی و معنوی این محصول متعلق به شرکت</span>{" "}
+				<span className="hidden sm:block">کلیه حقوق مادی و معنوی این محصول متعلق به شرکت</span>{" "}
 				<i className="px-2">
 					{" "}
-					<a href="https://samarayaneh.ir/" target="_blank" className="py-1 text-blue-400 hover:text-blue-300 hover:underline">
+					<a href="https://samarayaneh.ir/" target="_blank" className="text-blue-400 hover:text-blue-300 hover:underline">
 						سما رایانه
 					</a>
 				</i>{" "}
-				<span className="py-1 hidden sm:block me-1">میباشد</span>© {new Date().getFullYear()}
+				<span className="hidden sm:block me-1">میباشد</span>© {new Date().getFullYear()}
 			</div>
 		</>
 	);
