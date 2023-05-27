cb63ca29c5c5c1fe7d099610825500e3bf99f539
farshad-vgr <farshadvaghari@gmail.com>
2023-05-27 10:41:30 +0330

    footer elements edited

--- components/CopyRight/CopyRight.tsx
+++ components/CopyRight/CopyRight.tsx
@@ -1,11 +1,14 @@
 export default function CopyRight() {
 	return (
 		<>
-			<div className="flex items-center justify-center -translate-x-1/2 left-1/2 py-1 w-full h-10 text-white bg-black cursor-default">
+			<div className="absolute flex items-center justify-center -translate-x-1/2 left-1/2 py-1 w-full h-10 text-white bg-black cursor-default">
 				<span className="hidden sm:block">کلیه حقوق مادی و معنوی این محصول متعلق به شرکت</span>{" "}
-				<a href="https://samarayaneh.ir/" target="_blank" className="text-blue-400 hover:text-blue-300 hover:underline">
-					سما رایانه
-				</a>
+				<span className="px-2">
+					{" "}
+					<a href="https://samarayaneh.ir/" target="_blank" className="text-blue-400 hover:text-blue-300 hover:underline">
+						سما رایانه
+					</a>
+				</span>{" "}
 				<span className="hidden sm:block me-1">میباشد</span>© {new Date().getFullYear()}
 			</div>
 		</>
