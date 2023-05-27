65711277215a4f80b311396e71fb19d22e85ed62
farshad-vgr <farshadvaghari@gmail.com>
2023-05-27 09:58:17 +0330

    main content padding edited

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Container, CopyRight } from "../index";
 export default function Footer() {
 	return (
 		<>
-			<footer className="w-full p-1">
+			<footer className="w-full py-1">
 				<div className="w-full h-full text-white bg-black">
 					<Container>
 						<CopyRight />
