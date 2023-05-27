0933ae010b2d55c5c3fcb2f1c26137d888582188
farshad-vgr <farshadvaghari@gmail.com>
2023-05-27 10:45:59 +0330

    footer elements edited

--- components/CopyRight/CopyRight.tsx
+++ components/CopyRight/CopyRight.tsx
@@ -1,7 +1,7 @@
 export default function CopyRight() {
 	return (
 		<>
-			<div className="absolute flex items-center justify-center -translate-x-1/2 left-1/2 py-1 w-full h-10 text-white bg-black cursor-default">
+			<div className="absolute bottom-0 flex items-center justify-center -translate-x-1/2 left-1/2 py-1 w-full h-10 text-white bg-black cursor-default">
 				<span className="hidden sm:block">کلیه حقوق مادی و معنوی این محصول متعلق به شرکت</span>{" "}
 				<span className="px-2">
 					{" "}
